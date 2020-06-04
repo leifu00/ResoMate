82a6a660ef129a45d626f8d2663baed6727983a2
Lei Fu <lei.fu2020@gmail.com>
2020-06-04 22:23:12 +0100

    Fetched query info from db.

--- lib/components/posts_list.dart
+++ lib/components/posts_list.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _firestore = Firestore.instance;
 
 class PostsList extends StatefulWidget {
   @override
@@ -10,11 +13,46 @@ class _PostsListState extends State<PostsList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return Text(posts[index]);
+//    return ListView.builder(
+//      itemBuilder: (context, index) {
+//        return Text(posts[index]);
+//      },
+//      itemCount: 3,
+//    );
+    return Column(
+      children: <Widget>[
+        PostsStream(),
+      ],
+    );
+  }
+}
+
+class PostsStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('posts').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        final posts = snapshot.data.documents.reversed;
+        List<Text> postsList = [];
+        for (var post in posts) {
+          postsList.add(Text(post.data['type']));
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: postsList,
+          ),
+        );
       },
-      itemCount: 3,
     );
   }
 }
