a5a17a5ece7f92db949955d7132ac6f36beeb8f2
Lei Fu <lei.fu2020@gmail.com>
2020-06-09 14:54:24 +0100

    Added Page Navigator for 0609_assignment.

---
+++ lib/screens/copyright_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CopyrightScreen extends StatefulWidget {
+  static const String id = 'copyright_screen';
+
+  // TODO: Implement Copyright Screen Layout and Styles
+  @override
+  _CopyrightScreenState createState() => _CopyrightScreenState();
+}
+
+class _CopyrightScreenState extends State<CopyrightScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  static const String id = 'profile_screen';
+
+  // TODO: Implement Profile Screen Layout and Styles
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
