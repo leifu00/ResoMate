a33c3de6fbef9772bc67634a81315d5d5a1c9ed4
wf4118 <wf4118@imperial.ac.uk>
2020-06-10 18:28:17 +0800

    issue #27 - Modify Trending Page Styles'

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -64,8 +64,10 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFAFAFA),
       appBar: AppBar(
-        backgroundColor: Colors.grey,
+        backgroundColor: Color(0xFF4C6056),
+        leading: Icon(Icons.graphic_eq, color: Color(0xFFFFFFFF),),
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
@@ -90,9 +92,11 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ],
         currentIndex: _selectedIndex,
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.black,
+        backgroundColor: Color(0xFF6FA06C),
+        unselectedItemColor: Color(0xFF2E2F2E),
+        selectedItemColor: Color(0xFFFAFAFA),
         onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
